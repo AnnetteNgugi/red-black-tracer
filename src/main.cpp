3258edb5b6cc270f07dee07a0c4248b9d4f4da59
Annette <annette.ngugi@student.moringaschool.com>
2026-06-08 15:24:55 +0300

    create command-line demo application

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "../include/RedBlackTree.h"
+
+int main()
+{
+    RedBlackTree tree;
+
+    tree.insert(10);
+    tree.insert(20);
+    tree.insert(30);
+    tree.insert(15);
+    tree.insert(25);
+    tree.insert(5);
+
+    return 0;
+}
